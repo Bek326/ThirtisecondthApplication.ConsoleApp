90ac80c24381929105b937011f8aa6d6caac4cb3
Name <email@example.ru>
2021-09-21 15:30:03 +0600

    Добавьте файлы проекта.

---
+++ ThirtisecondthApplication.ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ThirtisecondthApplication.ConsoleApp
+{
+	public class User
+	{
+		public string Login { get; set; }
+		public string Name { get; set; }
+		public bool IsPremium { get; set; }
+		static void ShowAds()
+		{
+			Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+			Thread.Sleep(1000);
+
+			Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+			Thread.Sleep(2000);
+
+			Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+			Thread.Sleep(3000);
+		}
+		public void Site()
+        {
+			string name = Console.ReadLine();
+            switch(name)
+			{
+				case "Подписка":
+					Console.WriteLine($"Здравствуйте, {Login}, у вас есть {Name}");
+					break;
+				case "Нет подписки":
+					Console.WriteLine($"Здравствуйте, {Login}, у вас нету {Name}");
+					ShowAds();
+					break;
+			}
+				
+        }
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var user = new User{Login = "Bek326", Name = "премиум-подписка", IsPremium = true};
+			user.Site();
+			Console.ReadKey();
+		}
+	}
+}
